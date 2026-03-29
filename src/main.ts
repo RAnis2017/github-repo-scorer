7e54d62c6b72f15dd319dc68948ca764da7b517f
Raza Anis <razaanis123@gmail.com>
2026-03-29 12:40:00 +0400

    docs: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('GitHub Repo Scorer')
+    .setDescription('Search and score GitHub repositories by popularity')
+    .setVersion('1.0')
+    .build();
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, config));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
